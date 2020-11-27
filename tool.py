c08b5d2577139d9d546080cca6e5ca901e31f158
ist186410 <diogo.f.fernandes@tecnico.ulisboa.pt>
2020-11-27 16:37:52 +0000

    Fix source attribution in callExpr

--- tool.py
+++ tool.py
@@ -31,10 +31,11 @@ class VarObj:
         for s in sans:
             if s not in self.sans:
                 self.sans.appen(s)
+      
+    def setValues(self, taint, sources, sans):
+        self.setSources(taint, sources)
+        self.setSans(taint, sans)
 
-
-print("program.json:\n", program)
-print("\npattern.json:\n", pattern)
 
 filename = str(program.split(".")[0])
 
@@ -42,56 +43,12 @@ f = open(pattern, "r")
 pattern_str = f.read()
 f.close()
 pattern_list = json.loads(pattern_str)
-#print(pattern_str)
-#print(json.loads(pattern_str))
-#print(json.loads(pattern_str)[0]['vulnerability'])
 
 f = open(program, "r")
 program_str = f.read()
 f.close()
 program_json = json.loads(program_str)
-print(program_str)
 
-'''
-for pat in pattern_list:
-    sources = pat['sources']
-    sinks = pat['sinks']
-    sanitizers = pat['sanitizers']
-    for var_json in program_json['body']:
-        expr = var_json['expression']
-        if expr['type'] == 'CallExpression':
-            callee = expr['callee']['name']
-            args = expr['arguments']['name']
-            if callee in sources:
-                
-            for arg in args:
-                if arg in sinks:
-                    vardict[callee] += [arg]
-                if var in sinks: #might be source and sink? otherwise elif
-                    
-                if vardict[expr['argument']['name']] == "t":
-                    output.append(pat['name'])
-
-
-            else:
-                vardict[var] =""
-            
-
-        elif expr['type'] == 'AssignmentExpression':
-            print("left: ", expr['left']['name'])
-            var = expr['left']['name']
-            if var in sources:
-                varlist.append(VarObj(var, "t"))
-            else:
-                varlist.append(VarObj(var, "u"))
-            
-'''              
-
-
-for i in range(0, len(program_json['body'])):
-    
-    print("\n\n", program_json['body'][i])
-    print("\n\n",program_json.keys())
 
 
 def analyseSlice(pattern_list, program_json):
@@ -100,12 +57,12 @@ def analyseSlice(pattern_list, program_json):
         for var_json in program_json['body']:
             stmt = var_json    
             parser(stmt, pat)
-    print(flows)    
+        
+    print(flows)
+    #print("a:", vardict['b'].name, vardict['b'].taint, vardict['b'].sources, vardict['b'].sans)
 
 def parser(exprstmt, pattern):
-# types: expressionStmt -> assignment,call,bin
-# ifStmt -> test
-    if exprstmt['type'] == 'ExpressionStatement':
+   if exprstmt['type'] == 'ExpressionStatement':
         exprStmtParser(exprstmt['expression'], pattern)
 
 
@@ -119,27 +76,28 @@ def exprStmtParser(expr, pattern):
 def assignExprParser(expr, pattern):
     left = expr['left']
     right = expr['right']
-    if right['type'] == 'Literal':
-        if left['name'] not in vardict.keys():
-            vardict[left['name']] = VarObj(left['name'], 'u', [], [])
-    elif right['type'] == 'CallExpression':
+    if left['name'] not in vardict.keys():
+        vardict[left['name']] = VarObj(left['name'], 'u', [], [])
+    if right['type'] == 'CallExpression':
         callExprParser(right, pattern)
+        if vardict[right['callee']['name']].taint != 'u':
+            var_right = vardict[right['callee']['name']]
+            vardict[left['name']].setValues(var_right.taint, var_right.sources, var_right.sans)
 
-# dict = {varname : [taintedness, list of sources]
+
+
 def callExprParser(expr, pat):
     global flows #FIXME: remove this
     callee = expr['callee']['name']
     if callee not in vardict.keys():
         vardict[callee] = VarObj(callee, 'u', [], [])
     if callee in pat['sources']:
-        #vardict[callee] = ['t', [callee]]
         vardict[callee].setSources('t', [callee])
     for arg in expr['arguments']:
         #if callExpression...
         if arg['name'] not in vardict.keys():
             vardict[arg['name']] = VarObj(arg['name'], 't', [arg['name']], []) #FIXME: check if default should be tainted
         if arg['name'] in pat['sources']:
-            #vardict[arg['name']] = ['t', [arg['name']]
             vardict[arg['name']].setSources('t', [arg['name']])
             vardict[callee].setSources('t', [arg['name']]) 
         elif vardict[arg['name']].taint == 't':
@@ -148,7 +106,6 @@ def callExprParser(expr, pat):
     if callee in pat['sinks']:
         for arg in expr['arguments']:
             if vardict[arg['name']].taint == 't':
-                #vardict[callee].setFlow(pat.vuln, vardict[callee].sources, vardict[callee].name, [])
                 flows += [[pat['vulnerability'], vardict[callee].sources, vardict[callee].name, []]]
             #elif vardict[arg['name'].taint == 's':
                 #todo
