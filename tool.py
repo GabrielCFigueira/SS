f0ff661a689e96393ee012aebeb65eb1f1d4a691
Gabriel Figueira <gabriel.figueira@tecnico.ulisboa.pt>
2020-11-27 21:58:17 +0000

    PO + Compiladores

--- tool.py
+++ tool.py
@@ -14,12 +14,10 @@ global flows #FIXME: just testing if list works
 flows = []
 
 class VarObj:
-    def __init__(self, name, taint, sources, sans):
+    def __init__(self, name, taint):
         self.name = name
         self.taint = taint
-        self.sources = sources
-        self.sans = sans
-
+    """
     def setSources(self, taint, sources):
         self.taint = taint
         for s in sources:
@@ -35,6 +33,118 @@ class VarObj:
     def setValues(self, taint, sources, sans):
         self.setSources(taint, sources)
         self.setSans(taint, sans)
+    """
+
+class Taint:
+    def __init__(self, state, sources, sans):
+        self.state = state
+        self.sources = sources
+        self.sans = sans
+
+class Node:
+    def __init__(self):
+        self.taint = Taint("u", [], [])
+
+
+class Literal(Node):
+
+    def __init__(self, node):
+        super().__init__()
+        self.value = node['value']
+
+    def parse(self, pattern):
+        pass
+
+class Variable(Node):
+
+    def __init__(self, node):
+        super().__init__()
+        self.name = node['name']
+
+    def parse(self, pattern):
+        if self.name not in vardict.keys():
+            if self.name in pattern['sources']:
+                taint = Taint("t", [self.name], [])
+                vardict[self.name] = VarObj(self.name, taint)
+            else:
+                vardict[self.name] = VarObj(self.name, Taint("u", [], []))
+        
+        self.taint = vardict[self.name].taint
+
+
+class ExpressionStatement(Node):
+
+    def __init__(self, node):
+        super().__init__()
+        if node['type'] == 'AssignmentExpression':
+            self.expression = AssignmentExpression(node)
+        elif node['type'] == 'CallExpression':
+            self.expression = CallExpression(node)
+        elif node['type'] == "Identifier":
+            self.expression = Variable(node)
+        elif node['type'] == "Literal":
+            self.expression = Literal(node)
+        else:
+            raise ValueError("Shoud have never come here")
+
+    def parse(self, pattern):
+        self.expression.parse(pattern)
+        self.taint = self.expression.taint
+
+
+class AssignmentExpression(Node):
+
+    def __init__(self, node):
+        super().__init__()
+        left = node['left']
+        right = node['right']
+        
+        if left['type'] == "Identifier": #FIXME can left be anything else?
+            self.left = Variable(left)
+        else:
+            raise ValueError("Shoud have never come here")
+
+        self.right = ExpressionStatement(right)
+
+    def parse(self, pattern):
+        self.left.parse(pattern)
+        self.right.parse(pattern)
+
+        self.taint = self.right.taint #FIXME what about sans, also, check sinks?
+        vardict[self.left.name].taint = self.right.taint
+
+class CallExpression(Node):
+
+    def __init__(self, node):
+        super().__init__()
+        callee = node['callee']
+
+        if callee['type'] == "Identifier": #FIXME can left be anything else?
+            self.callee = Variable(callee)
+        else:
+            raise ValueError("Shoud have never come here")
+
+        self.arguments = []
+        for arg in node['arguments']:
+            self.arguments += [ExpressionStatement(arg)]
+
+    def parse(self, pattern):
+        self.callee.parse(pattern)
+        print(self.callee.taint.state)
+
+        self.taint = self.callee.taint
+        for arg in self.arguments:
+            arg.parse(pattern)
+            if arg.taint.state == "t" or arg.taint.state == "s" and self.taint.state == "u":
+                self.taint = arg.taint #FIXME check if callee is sink
+
+        if self.taint.state == "t" and self.callee.name in pattern['sanitizers'] and vardict[self.callee.name].taint.state != "t": 
+            self.taint = Taint("s", self.taint.sources, self.taint.sans + [self.callee.name])
+
+        #check if callee is sink FIXME or should we check in variable node?
+
+
+
 
 
 filename = str(program.split(".")[0])
@@ -52,19 +162,19 @@ program_json = json.loads(program_str)
 
 
 def analyseSlice(pattern_list, program_json):
-    global flows #FIXME remove this
     for pat in pattern_list:
+        program = []
         for var_json in program_json['body']:
-            stmt = var_json    
-            parser(stmt, pat)
-            #print("a:", vardict['a'].name, vardict['a'].taint, vardict['a'].sources, vardict['a'].sans)   
+            program += [ExpressionStatement(var_json["expression"])]
+        for p in program:
+            p.parse(pat)
 
-        
-    print("flows: ",flows)
-    print("afin:", vardict['a'].name, vardict['a'].taint, vardict['a'].sources, vardict['a'].sans)
-    print("bfin:", vardict['b'].name, vardict['b'].taint, vardict['b'].sources, vardict['b'].sans)
-    print("dfin:", vardict['d'].name, vardict['d'].taint, vardict['d'].sources, vardict['d'].sans)   
 
+    print("afin:", vardict['a'].name, vardict['a'].taint.state)
+    print("bfin:", vardict['b'].name, vardict['b'].taint.state)
+    print("dfin:", vardict['d'].name, vardict['d'].taint.state)   
+
+'''
 
 def parser(exprstmt, pattern):
    if exprstmt['type'] == 'ExpressionStatement':
@@ -122,11 +232,12 @@ def callExprParser(expr, pat):
             if 
             vardict[arg['name']].setValues('c', vardict[arg['name']].sources, [callee])
         if vardict[arg['name']].taint != 'u':
+            pass
 
             
                  
            
-    '''
+    
     if callee not in vardict.keys():
         vardict[callee] = VarObj(callee, 'u', [], [])
     if callee in pat['sources']:
@@ -169,7 +280,7 @@ def callExprParser(expr, pat):
 
 
 
-
+'''
 
 
 
